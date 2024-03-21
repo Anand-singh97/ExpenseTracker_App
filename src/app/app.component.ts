187ffafd60ac41dd0cdc5ab79b45b9a3ce663138
Anandpravesh Singh <Asingh4337@conestogac.on.ca>
2024-03-21 17:59:53 -0400

    slider added and dummy data in service initialized.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {RouterOutlet} from '@angular/router';
+import {NavbarComponent} from "./navbar/navbar.component";
+import {AnalyticsComponent} from "./analytics/analytics.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+  imports: [CommonModule, RouterOutlet, NavbarComponent, AnalyticsComponent],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'AngularBootstrapTemplate';
+    title = 'AngularBootstrapTemplate';
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,14 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { Routes } from '@angular/router';
+import { ChartComponent } from './chart/chart.component';
 
-import { routes } from './app.routes';
+// Define your routes
+export const routes: Routes = [
+  // Define your routes here if needed
+];
 
-export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+// Define your application configuration object
+export const appConfig = {
+  declarations: [ChartComponent], // Declare your components
+  providers: [], // Provide any services if needed
+  routes: routes // Define your routes
 };
