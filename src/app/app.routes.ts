7c87021b1c372346d0c8bd158e17622853cc7938
Anandpravesh Singh <Asingh4337@conestogac.on.ca>
2024-04-05 04:18:37 -0400

    Routes fixed for add income/expense

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,18 @@ import { Routes } from '@angular/router';
 import {HomePageComponent} from "./home-page/home-page.component";
 import {IncomeFormComponent} from "./income-form/income-form.component";
 import {ExpenseFormComponent} from "./expense-form/expense-form.component";
+import {TransactionsPageComponent} from "./transactions-page/transactions-page.component";
 export const routes: Routes = [
   {path:'home', component: HomePageComponent},
   {path:'', component: HomePageComponent},
-  {path:'transaction/income', component: IncomeFormComponent},
-  {path:'transaction/expense', component: ExpenseFormComponent},
+  // {path:'transaction/income/:id', component: IncomeFormComponent},
+  // {path:'transaction/expense/:id', component: ExpenseFormComponent},
+  {
+    path: 'transaction',
+    component: TransactionsPageComponent,
+    children: [
+      { path: 'income', component: IncomeFormComponent },
+      { path: 'expense', component: ExpenseFormComponent }
+    ]
+  }
 ];
